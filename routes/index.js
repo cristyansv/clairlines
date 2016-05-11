08a08dee5ec8b5f53e0d04181ac6bc086fe52c98
lauramv21 <laura_mv21@hotmail.com>
2016-05-11 17:43:14 -0500

    api

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : '207.223.165.157',
+  user     : 'clairlines',
+  password : '22clairlines',
+  database : 'clairlines'
+});
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/query', function (req, res) {
+
+  console.log(req.body.query);
+
+  connection.query(req.body.query, function(err, rows) {
+      if(err){
+        res.send(err);
+      }else {
+        res.send(rows);
+      }
+  });
+});
+
+
+
+
 
 module.exports = router;
