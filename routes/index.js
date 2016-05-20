ef3ad3ff867a3bed1a2044447df3157c322aba01
lauramv21 <laura_mv21@hotmail.com>
2016-05-20 09:55:44 -0500

    agregar empleado,aeropuerto, avion
    Los valores de pasajero cambiaron de nombre, así que no si hacer la
    función de nuevo

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,135 @@ router.get("/getUserById/:id", function (req, res) {
 
 
 
+router.get("/getEmployeeById/:id", function (req, res) {
+  connection.query("Select * from Empleado where ID_pasajero = "+req.params.id, function(err, rows) {
+    if(err){
+      res.send(err);
+    }else {
+      res.send(rows);
+    }
+  });
+});
+
+
+
+router.post('/nuevoPasajero', function (req, res) {
+
+
+  var nombre = req.body.nombre;
+  var cedula = req.body.cedula;
+
+  var id = 99;
+
+  console.log(nombre, cedula);
+
+
+  var consulta = "insert into Pasajero (ID_pasajero, Nombre, Cedula) values ("+id+","+nombre+","+cedula+")";
+
+
+  connection.query(consulta, function(err, rows) {
+    if(err){
+      res.send(err);
+    }else {
+      res.send(rows);
+    }
+  });
+});
+
+
+
+
+module.exports = router;
+
+
+router.post('/nuevoAvion', function (req, res) {
+
+
+  var nroasientos = req.body.nroasientos;
+  var disponibilidad = req.body.disponibilidad;
+  var modelo = req.body.modelo;
+
+  var id = 99;
+
+  console.log(nroasientos, disponibilidad, modelo);
+
+
+  var consulta = "insert into Avion (idavion, nroasientos, disponibilidad, modelo) values ("+idavion+", "+nroasientos+","+disponibilidad+","+modelo+")";
+
+
+  connection.query(consulta, function(err, rows) {
+    if(err){
+      res.send(err);
+    }else {
+      res.send(rows);
+    }
+  });
+});
+
+
+
+
+module.exports = router;
+
+
+router.post('/nuevoEmpleado', function (req, res) {
+
+
+  var nombre = req.body.nombre;
+  var cedula = req.body.cedula;
+  var apellido = req.body.apellido;
+  var cargo = req.body.cargo;
+  var tipo = req.body.tipo;
+  var categoria = req.body.categoria;
+
+  var id = 99;
+
+  console.log(nombre, cedula, apellido, cargo, tipo, categoria);
+
+
+  var consulta = "insert into Empleado (idempleado, nombre, cedula, apellido, cargo, tipo, categoria) values ("+idempleado+", "+nombre+","+cedula+","+apellido+", "+cargo+", "+tipo+", "+categoria+")";
+
+
+  connection.query(consulta, function(err, rows) {
+    if(err){
+      res.send(err);
+    }else {
+      res.send(rows);
+    }
+  });
+});
+
+
+
+
+module.exports = router;
+
+
+router.post('/nuevoAeopuerto', function (req, res) {
+
+
+  var nombreaeropuerto = req.body.nombreaeropuerto;
+  var ciudad = req.body.ciudad;
+
+
+  var id = 99;
+
+  console.log(nombreaeropuerto, ciudad);
+
+
+  var consulta = "insert into Aeropuerto (idearopuerto, nombreaeropuerto, ciudad) values ("+idaeropuerto+", "+nombreaeropuerto+","+ciudad+")";
+
+
+  connection.query(consulta, function(err, rows) {
+    if(err){
+      res.send(err);
+    }else {
+      res.send(rows);
+    }
+  });
+});
+
+
 
 
 module.exports = router;
