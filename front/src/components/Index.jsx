089bc48ad7ae66c67e97d9e6ea0eeed83f4c84de
Cristyan <cristyan.sv@gmail.com>
2016-05-21 14:56:28 -0500

    basic

---
+++ front/src/components/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Index extends React.Component {
+    render() {
+        return (
+            <h1>hi</h1>
+        )
+    }
+}
+
+export default Index;
---
+++ front/src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from'react-dom';
+
+import Index from './components/Index.jsx';
+
+ReactDOM.render(<Index />, document.getElementById('App'));
