cf443136d1c6d401059efbd12616460d6b188758
Cristyan <cristyan.sv@gmail.com>
2016-05-21 18:00:36 -0500

    background

--- front/src/components/Bar.jsx
+++ front/src/components/Bar.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
 
+
+var Style ={
+    backgroundColor: 'RGBA(0,0,0,0.3)'
+};
+
 const Bar = () => (
     <AppBar
+        style={Style}
         title="CL Airlines"
         iconClassNameRight="muidocs-icon-navigation-expand-more"
     />
