2bf9890e89d699c15cc7ddfa199fa345bacaab14
Cristyan <cristyan.sv@gmail.com>
2016-05-21 17:33:35 -0500

    mum theme

--- front/src/Routes.jsx
+++ front/src/Routes.jsx
@@ -3,11 +3,18 @@ import { Router, Route, hashHistory } from 'react-router'
 import Index from './components/Index.jsx';
 import Aviones from './components/Aviones.jsx';
 
+
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+
+
 var Routes = (
-    <Router history={hashHistory}>
-        <Route path="/" component={Index}/>
-        <Route path="/aviones" component={Aviones}/>
-    </Router>
+    <MuiThemeProvider muiTheme={getMuiTheme()}>
+        <Router history={hashHistory}>
+            <Route path="/" component={Index}/>
+            <Route path="/aviones" component={Aviones}/>
+        </Router>
+    </MuiThemeProvider>
 );
 
 export default Routes;
