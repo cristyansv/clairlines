70d11e205c054dafc71c7c8b53ff673cd97e5e1a
Cristyan <cristyan.sv@gmail.com>
2016-05-21 17:43:05 -0500

    appbar

--- front/src/Routes.jsx
+++ front/src/Routes.jsx
@@ -7,13 +7,18 @@ import Aviones from './components/Aviones.jsx';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
+import Bar from './components/Bar.jsx';
+
 
 var Routes = (
     <MuiThemeProvider muiTheme={getMuiTheme()}>
-        <Router history={hashHistory}>
-            <Route path="/" component={Index}/>
-            <Route path="/aviones" component={Aviones}/>
-        </Router>
+        <div>
+            <Bar />
+            <Router history={hashHistory}>
+                <Route path="/" component={Index}/>
+                <Route path="/aviones" component={Aviones}/>
+            </Router>
+        </div>
     </MuiThemeProvider>
 );
 
---
+++ front/src/components/Bar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+
+const Bar = () => (
+    <AppBar
+        title="CL Airlines"
+        iconClassNameRight="muidocs-icon-navigation-expand-more"
+    />
+);
+
+export default Bar;
